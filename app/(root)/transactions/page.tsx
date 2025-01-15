0deb8bee9d0d778ffff31a5f03ee2b2b0488ab8c
Martin Leconte <109480591+Epineux@users.noreply.github.com>
2025-01-16 03:35:51 +0900

    new fix

--- app/(root)/transactions/page.tsx
+++ app/(root)/transactions/page.tsx
@@ -1,6 +1,7 @@
 import { columns } from "@/components/Transactions/transactionsColumns";
 import TransactionsTable from "@/components/Transactions/TransactionsTable";
 import data from "@/constants/data.json";
+import { Suspense } from "react";
 
 // Async function getData()
 const page = () => {
@@ -10,7 +11,9 @@ const page = () => {
     <main className="px-md py-xl @container sm-490:px-3xl sm-490:py-2xl">
       <h1 className="h1 text-grey-900">Transactions</h1>
       <section className="mt-2xl">
-        <TransactionsTable columns={columns} data={transactionsData} />
+        <Suspense fallback={<div>Loading...</div>}>
+          <TransactionsTable columns={columns} data={transactionsData} />
+        </Suspense>
       </section>
     </main>
   );
